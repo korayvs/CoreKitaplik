7e8b65ec6b469feac6829f443a99de15e59fc7d6
Koray Vatansever <koray-vatansever@hotmail.com>
2025-03-10 20:44:40 +0300

    Add project files.

---
+++ CoreKitaplik/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using CoreKitaplik.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreKitaplik.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar(){Id = 1, KitapAd = "Satranç", Yazar = "Zweig" },
+                new Kitaplar(){Id = 2, KitapAd = "Dr Ox'un Deneyi", Yazar = "Jules Verne" },
+                new Kitaplar(){Id = 3, KitapAd = "Delifişek", Yazar = "Vasconcelos" },
+                new Kitaplar(){Id = 4, KitapAd = "Kırmızı Bisiklet", Yazar = "Muzaffer İzgü" },
+                new Kitaplar(){Id = 5, KitapAd = "Toros Canavarı", Yazar = "Aziz Nesin" }
+            };
+            return View(ktp);
+        }
+    }
+}
---
+++ CoreKitaplik/Models/Kitaplar.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreKitaplik.Models
+{
+    public class Kitaplar
+    {
+        public int Id { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
+    }
+}
